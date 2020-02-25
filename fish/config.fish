6b55f4f4ec4a36f76d947ded314f37c82e68e4f0
Matt Bailey <matt.bailey@bethesda.net>
2020-02-25 12:20:38 -0800

    export fish vars

--- fish/config.fish
+++ fish/config.fish
@@ -1,30 +1,30 @@
 
-set PATH $HOME/.config/scripts $PATH
-set PATH $HOME/go/bin $PATH
-set PATH /usr/local/opt/go/libexec/bin $PATH
+set -x PATH $HOME/.config/scripts $PATH
+set -x PATH $HOME/go/bin $PATH
+set -x PATH /usr/local/opt/go/libexec/bin $PATH
 
-# Setting EDITOR
+# set -xting EDITOR
 if type -q vi
-  set EDITOR vi
+  set -x EDITOR vi
 end
 if type -q vim
-  set EDITOR vim
+  set -x EDITOR vim
 end
 if type -q nvim
-  set EDITOR nvim
+  set -x EDITOR nvim
   alias vim nvim
 end
 
 # Go
-set GOPATH $HOME/go
-set CGO_ENABLED 0
-set GO111MODULE on
+set -x GOPATH $HOME/go
+set -x CGO_ENABLED 0
+set -x GO111MODULE on
 
 # bat, cat replacement
 # https://github.com/eth-p/bat-extras
 if type -q bat
   alias cat bat
-  set PAGER bat
+  set -x PAGER bat
 end
 if type -q batgrep
   alias rg batgrep
@@ -37,6 +37,6 @@ if type -q batwatch
 end
 
 # GHQ repo manager
-set GHQ_ROOT $HOME/src
+set -x GHQ_ROOT $HOME/src
 
 starship init fish | source
